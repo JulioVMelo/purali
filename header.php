c5068d3012975371b57a268f9635d855952b1fc8
Julio Melo <juliovilar93@gmail.com>
2017-07-28 14:34:57 -0300

    Commit dos arquivos footer, header,index. Linhas do header criadas

---
+++ header.php
@@ -0,0 +1,49 @@
+<?php wp_head(); ?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+
+        <title> <?php bloginfo( 'title' ); ?> </title>
+
+        <meta name="description" content="">
+
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <link rel="stylesheet" href="style.css">
+
+        <!-- Latest compiled and minified CSS -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+        <!-- Latest compiled and minified JavaScript -->
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+    </head>
+        <div class="container-fluid">
+            <header>
+                <div class="row content">
+                    <div class="col-md-3" style="background-color:blue;">
+                        PURALI
+                    </div>
+                    <div class="col-md-6" style="background-color:red; text-align:center;">
+                        teste2
+                    </div>
+                    <div class="col-md-3" style="background-color:green;">
+                        teste3
+                    </div>
+                </div>
+                <div class="row content">
+                    <div col-md-5>
+
+                    </div>
+                    <div col-md-7>
+
+                    </div>
+                </div>
+                <div class="row">
+                    <div col-md-12>
+
+                    </div>
+                </div>
+            </header>
+        </div>
+    <body>
+        
