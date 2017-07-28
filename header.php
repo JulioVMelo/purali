aeac588e95d97954dcb6086c3747e56907c8dbed
Julio Melo <juliovilar93@gmail.com>
2017-07-28 15:27:29 -0300

    bootstrap retirado do projeto

--- header.php
+++ header.php
@@ -12,35 +12,29 @@
 
         <link rel="stylesheet" href="style.css">
 
-        <!-- Latest compiled and minified CSS -->
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-        <!-- Latest compiled and minified JavaScript -->
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
     </head>
-        <div class="container-fluid">
+        <div class="container">
             <header>
-                <div class="row content">
-                    <div class="col-md-3" style="background-color:blue;">
-                        PURALI
+                <div class="content">
+                    <div class="logo">
+                        <img src="<?php echo get_stylesheet_directory_uri(); ?>/logopurali.png">
                     </div>
-                    <div class="col-md-6" style="background-color:red; text-align:center;">
-                        teste2
+                    <div class="menu">
+                        <nav>
+                            <ul>
+                                <li><a>A PURALI</a></li>
+                                <li><a>PRODUTOS</a></li>
+                                <li><a>RECEITAS</a></li>
+                                <li><a>CONTATO</a></li>
+                            </ul>
+                        </nav>
                     </div>
-                    <div class="col-md-3" style="background-color:green;">
-                        teste3
-                    </div>
-                </div>
-                <div class="row content">
-                    <div col-md-5>
-
-                    </div>
-                    <div col-md-7>
-
-                    </div>
-                </div>
-                <div class="row">
-                    <div col-md-12>
-
+                    <div class="social">
+                        <ul>
+                            <li><a>Facebook</a></li>
+                            <li><a>Twitter</a></li>
+                            <li><a>Instagram</a></li>
+                        </ul>
                     </div>
                 </div>
             </header>
